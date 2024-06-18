b00db13e2813058f349e48b02018d7306a5d8771
SophiaKolesnikova <sophiakolesnikova.97@gmail.com>
2024-06-18 21:44:20 +0300

    Add login page

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from '../Atoms/Title/Title.tsx';
+
+const Auth: React.FC = () => {
+    return (
+        <div>
+            <Title size={'large'} variant={'secondary'}>
+                Login
+            </Title>
+        </div>
+    );
+};
+
+export default Auth;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header.tsx';
+import Auth from '../../components/Auth/Auth.tsx';
+
+const Login: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <Auth />
+        </>
+    );
+};
+
+export default Login;
